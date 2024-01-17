26b7ff2d97a80b0d1c40f7f2d16d9fb5da00cae3
AriExists <92820995+AriExists@users.noreply.github.com>
2024-01-17 11:51:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function pageChange(page){
     switch (page) {
         case 0:
-            window.location.replace("http://127.0.0.1:5500/Index.html");
+            window.location.replace("http://127.0.0.1:5500/index.html");
             break;
         
         case 1:
